b105eb3a14a961644f5565e4c8a704fee80dedc8
ArutyunovG <ArutyunovG@yandex.ru>
2020-01-23 19:06:49 +0300

    update torchvision

--- frameworks/pytorch/torchvision/install_torchvision.sh
+++ frameworks/pytorch/torchvision/install_torchvision.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+TORCHVISION_VERSION=0.5.0
+
 git clone --recursive https://github.com/pytorch/vision.git
-cd vision && git checkout master
+cd vision && git checkout v$TORCHVISION_VERSION
 python setup.py install
 cd .. && rm -rf vision
