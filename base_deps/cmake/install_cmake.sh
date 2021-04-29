25ec5976eb0f6f136c7727cbf7304bf17ee9c2ba
g-arutyunov <grigory.arutyunov@iss.ru>
2021-04-29 15:05:34 +0300

    update_versions

--- base_deps/cmake/install_cmake.sh
+++ base_deps/cmake/install_cmake.sh
@@ -3,8 +3,10 @@
 mkdir -p $UTILS_BASE
 
 wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
-tar -zxvf cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
-rm cmake-$CMAKE_VERSION-Linux-x86_64.tar.gz
-mv cmake-$CMAKE_VERSION-Linux-x86_64 $UTILS_BASE/cmake
+cmake_archive=$(ls | grep -E "cmake-"$CMAKE_VERSION"-[L|l]inux-x86_64" )
+tar -zxvf $cmake_archive
+rm $cmake_archive
+cmake_extracted_folder=$(ls | grep -E "cmake-"$CMAKE_VERSION"-[L|l]inux-x86_64" )
+mv $cmake_extracted_folder $UTILS_BASE/cmake
 
 echo 'export PATH=$PATH:$UTILS_BASE/cmake/bin' >> $SETUP_SCRIPT
