12dab4e177db3309bbbdbc043803f3a1d1d38737
g-arutyunov <ArutyunovG@yandex.ru>
2024-07-05 08:32:32 -0400

    allow docker container to run with clearml-agent

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-source /root/cv_workspace/libs/setup.sh
-/bin/bash "$@"
 
+source /root/cv_workspace/libs/setup.sh
+
+if [[ $# -eq 0 ]]; then
+  exec "/bin/bash" -l
+else
+  exec "$@"
+fi
